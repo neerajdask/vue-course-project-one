37863b158233162e8c716737df29ea75bc85d17b
neerajdask <iamneerajdas@gmail.com>
2021-03-31 10:22:48 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <ul>
+    <li></li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Google your way out of trouble!',
+          link: 'https://google.CO.IN',
+        },
+      ],
+    };
+  },
+};
+</script>
