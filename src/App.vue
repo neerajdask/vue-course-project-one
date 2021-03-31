a5cb898d83508bb73ccd8652685b06fbaca75ee6
neerajdask <iamneerajdas@gmail.com>
2021-03-31 10:47:13 +0530

    Added more components and styling.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
-  <ul>
-    <li></li>
-  </ul>
+  <stored-resources :resources="storedResources"></stored-resources>
 </template>
 
 <script>
+import StoredResources from './components/learning-resources/StoredResources.vue';
+
 export default {
+  components: {
+    StoredResources,
+  },
   data() {
     return {
       storedResources: [
         {
           id: 'official-guide',
           title: 'Official Guide',
-          description: 'The official Vue.js documentation',
+          description: 'The official Vue.js documentation.',
           link: 'https://vuejs.org',
         },
         {
           id: 'google',
           title: 'Google',
-          description: 'Google your way out of trouble!',
-          link: 'https://google.CO.IN',
+          description: 'Learn to google...',
+          link: 'https://google.org',
         },
       ],
     };
   },
 };
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
